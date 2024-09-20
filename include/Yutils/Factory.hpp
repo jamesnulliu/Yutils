f3f53e8a6516c913bceb25409f7bb7585fe18c52
jamesnulliu <jamesnulliu@gmail.com>
2024-09-20 21:35:06 +0800

    Update Factory

--- include/Yutils/Factory.hpp
+++ include/Yutils/Factory.hpp
@@ -1,71 +1,90 @@
+#include <functional>
+#include <memory>
 #include <mutex>
 #include <optional>
 #include <shared_mutex>
+#include <stdexcept>
+#include <typeinfo>
 #include <unordered_map>
+#include <variant>
 
 #include "Yutils/System.hpp"
 
 namespace yutils
 {
-template <typename KeyT, typename CreatorT, bool ThreadSafe = false,
-          typename ContainerT = std::unordered_map<KeyT, CreatorT>>
-class Factory
+template <bool threadSafe, typename KeyT, typename ValT,
+          typename MapT = std::unordered_map<KeyT, ValT>>
+class Registry
 {
+public:
+    explicit Registry() = default;
+    Registry& operator=(const Registry&) = delete;
+
+public:
+    void add(const KeyT& key, const ValT& val)
+    {
+        if constexpr (threadSafe) {
+            std::unique_lock<std::shared_mutex> lock(m_mutex);
+        }
+        m_mapper.emplace(key, val);
+    }
+
+    void remove(const KeyT& key)
+    {
+        if constexpr (threadSafe) {
+            std::unique_lock<std::shared_mutex> lock(m_mutex);
+        }
+        m_mapper.erase(key);
+    }
+
+    ValT get(const KeyT& key) const
+    {
+        if constexpr (threadSafe) {
+            std::shared_lock<std::shared_mutex> lock(m_mutex);
+        }
+        auto it = m_mapper.find(key);
+        if (it != m_mapper.end()) {
+            return it->second;
+        }
+        throw std::runtime_error("Key not found.");
+    }
+
+private:
+    std::conditional_t<threadSafe, std::shared_mutex, std::monostate> m_mutex;
+    MapT m_mapper;
+};
+
+template <bool threadSafe, typename KeyT,
+          typename ValT = std::function<std::shared_ptr<void>()>,
+          typename MapT = std::unordered_map<KeyT, ValT>>
+class Factory : public Registry<threadSafe, KeyT, ValT, MapT>
+{
+public:
+    using BaseT = Registry<threadSafe, KeyT, ValT, MapT>;
+
 public:
     explicit Factory() = default;
     Factory& operator=(const Factory&) = delete;
 
 public:
-    void registerCreator(const KeyT& key, const CreatorT& creator)
+    template <typename T>
+    void add(const KeyT& key)
     {
-        if constexpr (ThreadSafe) {
-            std::unique_lock<std::shared_mutex> lock(m_mutex);
-        }
-        m_container[key] = creator;
+        this->BaseT::add(key, []() -> std::shared_ptr<void> {
+            return std::make_shared<T>();
+        });
     }
 
-    void unregisterCreator(const KeyT& key)
+    template <typename ObjT = void>
+    auto create(const KeyT& key)
     {
-        if constexpr (ThreadSafe) {
-            std::unique_lock<std::shared_mutex> lock(m_mutex);
+        auto creator = this->BaseT::get(key);
+        auto ptr = std::static_pointer_cast<ObjT>(creator());
+        if (!ptr) {
+            throw std::bad_cast();
         }
-        m_container.erase(key);
+        return ptr;
     }
-
-    template <typename... Args>
-    auto create(const KeyT& key, Args&&... args)
-    {
-        if constexpr (ThreadSafe) {
-            std::shared_lock<std::shared_mutex> lock(m_mutex);
-        }
-        using ValT =
-            decltype(m_container.begin()->second(std::forward<Args>(args)...));
-        auto it = m_container.find(key);
-        // Return the result of the creator function if key found.
-        if (it != m_container.end()) {
-            return std::optional<ValT>{it->second(std::forward<Args>(args)...)};
-        }
-        // Return empty optional if key not found.
-        return std::optional<ValT>{};
-    }
-
-    std::optional<CreatorT> getCreator(const KeyT& key) const
-    {
-        if constexpr (ThreadSafe) {
-            std::shared_lock<std::shared_mutex> lock(m_mutex);
-        }
-        auto it = m_container.find(key);
-        // Return the creator function if key found.
-        if (it != m_container.end()) {
-            return {it->second};
-        }
-        // Return empty optional if key not found.
-        return {std::nullopt};
-    }
-
-private:
-    mutable std::shared_mutex m_mutex;
-    ContainerT m_container;
 };
 
 }  // namespace yutils
