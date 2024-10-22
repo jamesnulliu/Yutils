d14129fd99e6356a99e50a9544ae41d014f45452
jamesnulliu <jamesnulliu@outlook.com>
2024-10-23 00:59:35 +0800

    Test Bad Format (#14)

--- src/Yutils/ArgParser.cpp
+++ src/Yutils/ArgParser.cpp
@@ -13,8 +13,7 @@ ArgParser::ArgParser(std::string_view argv0)
 {
     m_logger = spdlog::stdout_color_mt("yutils::ArgParser");
 
-    m_helpMessage =
-        spdlog::fmt_lib::format("Usage:\n    {} [options...]\nOptions:\n",
+    m_helpMessage =spdlog::fmt_lib::format("Usage:\n    {} [options...]\nOptions:\n",
                                 yutils::extractFilename(argv0));
 
     // Add the executable name to the help message.
